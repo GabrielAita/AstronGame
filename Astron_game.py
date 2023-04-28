74f6e27722799c9d943045d207a7b3272557c0f0
ViniBrancatti <78756275+ViniBrancatti@users.noreply.github.com>
2023-04-28 12:33:14 -0300

    Add files via upload

--- Astron_game.py
+++ Astron_game.py
@@ -7,18 +7,21 @@ screenRes =(1152,648)
 screen = pygame.display.set_mode(screenRes, 0 ,32)
 
 
-    
+
 def menu():
-    icone_sem_som = pygame.image.load('musica_off_onhover.png').convert_alpha()
+    pygame.mixer.music.load('AstronGame-main\musicas\musicamenu.mp3')
+    pygame.mixer.music.play()
+    background = pygame.transform.scale(pygame.image.load("AstronGame-main\Sprites\Icon_Menu\Background_logo.png"), screenRes)
+    icone_sem_som = pygame.image.load('AstronGame-main\Sprites\Icon_Menu\musica_off_normal.png').convert_alpha()
     icone_sem_som = pygame.transform.scale(icone_sem_som, (74,54))
-    icone_com_som = pygame.image.load('musica_on_onhover.png').convert_alpha()
+    icone_com_som = pygame.image.load('AstronGame-main\Sprites\Icon_Menu\musica_on_onhover.png').convert_alpha()
     icone_com_som = pygame.transform.scale(icone_com_som, (74,54))
+    icone_Instrucoes = pygame.image.load('AstronGame-main\Sprites\Icon_Menu\Instruções_normal.png').convert_alpha()
+    icone_Instrucoes = pygame.transform.scale(icone_Instrucoes, (294,78))
     icone_som = icone_com_som
-    icon_Iniciar = pygame.image.load('iniciar_normal.png').convert_alpha()
+    icon_Iniciar = pygame.image.load('AstronGame-main\Sprites\Icon_Menu\iniciar_normal.png').convert_alpha()
     icon_Iniciar = pygame.transform.scale(icon_Iniciar, (168,66))
-    icon_Logo = pygame.image.load('Logo_Astron.png').convert_alpha()
-    icon_Logo = pygame.transform.scale(icon_Logo, (384,96))
-    icon_sair = pygame.image.load('sair_normal.png').convert_alpha()
+    icon_sair = pygame.image.load('AstronGame-main\Sprites\Icon_Menu\sair_normal.png').convert_alpha()
     icon_sair = pygame.transform.scale(icon_sair, (102,66))
 
     intro = True
@@ -59,10 +62,12 @@ def menu():
                     pygame.quit()
                     quit()
             
+            screen.blit(background, (0, 0))
             screen.blit(icone_som,(1000,550))
-            screen.blit(icon_Logo,(screenRes[0]/2-200,screenRes[1]/2-200))
-            screen.blit(icon_Iniciar,(screenRes[0]/2-75,screenRes[1]/2))
-            screen.blit(icon_sair,(screenRes[0]/2-50,screenRes[1]/2+100))
+            screen.blit(icon_Iniciar,(screenRes[0]/2-500,screenRes[1]/2-90))
+            screen.blit(icon_sair,(screenRes[0]/2-500,screenRes[1]/2+65))
+            screen.blit(icone_Instrucoes,(screenRes[0]/2-500,screenRes[1]/2-10))
+            
             pygame.display.update()
         
 def game():
@@ -73,19 +78,23 @@ def game():
        for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
-                quit() 
+                quit()
             screen.fill((255,255,255))
             pygame.draw.rect(screen,(255,255,0),(90,90,100,100))
             
-            pygame.mixer.music.load('04 Hopeful Feeling.mp3')
+            pygame.mixer.music.load('AstronGame-main\musicas\Hopeful Feeling.mp3')
             pygame.mixer.music.play()
+
             pos = pygame.mouse.get_pos()
+
+           
             if pos[0] >= 90 and pos[0] <= 190 and pos[1] >= 90 and pos[1] <= 190:
                pygame.draw.rect(screen,(0,255,0),(90,90,100,100))
             if event.type == pygame.MOUSEBUTTONDOWN: 
                 if pos >= (90,190) and pos <= (190,90):
                     menu()
 
+            
             pygame.display.update()
 
 menu()
